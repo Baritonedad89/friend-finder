22d4c3e7104ad6cfc6c76bfc3eb853130e01c9a6
Hero's Dad <cody.monta@gmail.com>
2019-01-13 23:25:40 -0600

    api is talking

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,11 @@ module.exports = function (app) {
         res.json(friends)
     });
 
+    app.post('/api/friendslist', (req, res) => {
+        friends.push(req.body);
+    })
+
     app.get('/survey', (req, res) => {
         res.sendFile(path.join(__dirname, "../public/survey.html"))
-
     })
 }
